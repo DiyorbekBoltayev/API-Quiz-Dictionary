74f862d9b9c74957e01841c2c6d0ea138a28f811
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-22 19:17:40 +0500

    swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="My API description"
+ * )
+ */
+
+/**
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ */
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+
 }
