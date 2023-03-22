789d4b83574cada0d627ba19c869c2f20828a4b1
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2023-03-22 19:17:28 +0500

    swagger

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,11 +7,30 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="My API description"
+ * )
+ *
+ */
+
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
+     */
+
+
+    /**
+     * @OA\Post(
+     *     path="/login",
+     *     @OA\Response(response="200", description="Login")
+     * )
      */
     public function store(LoginRequest $request): Response
     {
